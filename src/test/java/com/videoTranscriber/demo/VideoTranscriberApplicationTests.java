7689cf5741bd52aabc514bb5bc6cc1607d8da1aa
DmitriiS-dev <dmitrii.sizikov577@gmail.com>
2024-12-30 19:41:45 +0000

    Initial Project Setup - First commit

---
+++ src/test/java/com/videoTranscriber/demo/VideoTranscriberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.videoTranscriber.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoTranscriberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
